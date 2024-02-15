1ba79c22c51cf58c2a738166346a99aec3472b86
rpm <ryan.macarthur@gmail.com>
2024-02-14 22:11:50 -0400

    lint

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,5 @@
-
-use log::Level;
-use log::{info, warn};
-use std::fs::File;
 use tokio::fs;
-use std::io;
-use std::io::Read;
-use std::path::Path;
+
 use eyre::{eyre, Result};
 use structopt::StructOpt;
 use tracing::debug;
@@ -21,12 +15,12 @@ async fn main() -> Result<(), NotaryServerError> {
     // Load command line arguments which contains the config file location
 
     let cli_fields: CliFields = CliFields::from_args();
-	 let eph = fs::read("/tmp/key.pem").await.expect("gramine key not found");
-	let pl = EcKey::private_key_from_pem(&eph).expect("s");
+    let eph = fs::read("/tmp/key.pem").await.expect("gramine key not found");
+    let pl = EcKey::private_key_from_pem(&eph).expect("s");
     let pkey = PKey::from_ec_key(pl.clone()).expect("ss");
     let private_key = pkey.private_key_to_pem_pkcs8().expect("key");
 
-   fs::write("/tmp/fixed_key.pem", private_key).await.expect("cant write");
+    fs::write("/tmp/fixed_key.pem", private_key).await.expect("cant write");
 
     let config: NotaryServerProperties = parse_config_file(&cli_fields.config_file)?;
 
@@ -34,7 +28,7 @@ async fn main() -> Result<(), NotaryServerError> {
 
     debug!(?config, "Server config loaded");
 
-   run_server(&config).await?;
+    run_server(&config).await?;
 
     Ok(())
 }
